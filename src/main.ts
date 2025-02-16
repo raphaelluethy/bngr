6462e1b07f0e876677d009b8cdf7ef1205d975d1
Raphael Lüthy <raphael.luethy@fhnw.ch>
2025-02-16 15:01:42 +0100

    refactor(main.ts): Format code in getBang and doRedirect functions

--- src/main.ts
+++ src/main.ts
@@ -3,48 +3,48 @@ import { BangsMap } from "./bangs.ts";
 import template from "./template.html?raw";
 
 function getBang() {
-	const url = new URL(window.location.href);
-	const query = url.searchParams.get("q")?.trim() || "";
-	if (!query) {
-		renderDefaultPage();
-		return null;
-	}
+  const url = new URL(window.location.href);
+  const query = url.searchParams.get("q")?.trim() || "";
+  if (!query) {
+    renderDefaultPage();
+    return null;
+  }
 
-	const defaultEngineUrl = url.searchParams.get("e") || "google";
-	const engineMatch = defaultEngineUrl.match(/([a-z0-9]+)/i);
-	const defaultEngine = engineMatch?.[0] || "g";
+  const defaultEngineUrl = url.searchParams.get("e") || "g";
+  const engineMatch = defaultEngineUrl.match(/([a-z0-9]+)/i);
+  const defaultEngine = engineMatch?.[0] || "g";
 
-	const match = query.match(/!([a-z0-9]+)/i);
-	const potentialBang = match?.[1];
-	const bangName = potentialBang || defaultEngine;
+  const match = query.match(/!([a-z0-9]+)/i);
+  const potentialBang = match?.[1];
+  const bangName = potentialBang || defaultEngine;
 
-	const bang = BangsMap.get(bangName);
+  const bang = BangsMap.get(bangName);
 
-	// Remove the first bang from the query
-	const cleanQuery = query.replace(/![a-z0-9]+\s*/i, "").trim();
+  // Remove the first bang from the query
+  const cleanQuery = query.replace(/![a-z0-9]+\s*/i, "").trim();
 
-	// Format of the url is:
-	// https://www.google.com/search?q={{{s}}}
-	const searchUrl = bang?.u.replace(
-		"{{{s}}}",
-		// Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
-		encodeURIComponent(cleanQuery).replace(/%2F/g, "/"),
-	);
-	if (!searchUrl) return null;
+  // Format of the url is:
+  // https://www.google.com/search?q={{{s}}}
+  const searchUrl = bang?.u.replace(
+    "{{{s}}}",
+    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
+    encodeURIComponent(cleanQuery).replace(/%2F/g, "/"),
+  );
+  if (!searchUrl) return null;
 
-	return searchUrl;
+  return searchUrl;
 }
 
 function doRedirect() {
-	const searchUrl = getBang();
-	if (!searchUrl) return;
-	window.location.replace(searchUrl);
+  const searchUrl = getBang();
+  if (!searchUrl) return;
+  window.location.replace(searchUrl);
 }
 
 function renderDefaultPage() {
-	const appDiv = document.getElementById("app");
-	if (!appDiv) return;
-	appDiv.innerHTML = template;
+  const appDiv = document.getElementById("app");
+  if (!appDiv) return;
+  appDiv.innerHTML = template;
 }
 
 doRedirect();
