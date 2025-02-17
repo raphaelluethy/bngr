c4e0cf4a8015eaa1e378624926eff805a04a58c6
Raphael Lüthy <raphael.luethy@fhnw.ch>
2025-02-17 21:04:23 +0100

    build(vite-config):feat): Add PWA plugin): update vite configuration to include VitePWA plugin and adjust options): add VitePWA plugin configurations and retain existing tailwindcss plugin configurations)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-	plugins: [tailwindcss()],
-	assetsInclude: ["**/src/*.html"],
-	build: {
-		rollupOptions: {
-			input: "index.html",
-		},
-		assetsDir: "assets",
-	},
+  plugins: [
+    tailwindcss(),
+    VitePWA({
+      injectRegister: "auto",
+    }),
+  ],
+  assetsInclude: ["**/src/*.html"],
+  build: {
+    rollupOptions: {
+      input: "index.html",
+    },
+    assetsDir: "assets",
+  },
 });
